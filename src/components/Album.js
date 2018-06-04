cd72c57310c239519a0fc08cf94680617aaa1408
Roxanne Garcia <Roxanne@Asterisk.local>
2018-06-03 21:25:29 -0700

    bloc-jams-react-audio-playback: completd

--- src/components/Album.js
+++ src/components/Album.js
@@ -9,7 +9,39 @@ class Album extends Component {
       return album.slug === this.props.match.params.slug
     });
 
-    this.state = { album: album };
+    this.state = {
+      album: album,
+      currentSong: album.songs[0],
+      isPlaying: false
+    };
+
+    this.audioElement = document.createElement('audio');
+    this.audioElement.src = album.songs[0].audioSrc;
+  }
+
+  play() {
+    this.audioElement.play();
+    this.setState({ isPlaying: true });
+  }
+
+  pause() {
+    this.audioElement.pause();
+    this.setState({ isPlaying: false });
+  }
+
+  setSong(song) {
+    this.audioElement.src = song.audioSrc;
+    this.setState({ currentSong: song });
+  }
+
+  handleSongClick(song) {
+    const isSameSong = this.state.currentSong === song;
+    if (this.state.isPlaying && isSameSong) {
+      this.pause();
+    } else {
+      if (!isSameSong) { this.setSong(song); }
+      this.play();
+    }
   }
 
   render() {
@@ -31,7 +63,7 @@ class Album extends Component {
           </colgroup>
           <tbody>
            {this.state.album.songs.map( (song, index) =>
-              <tr className="songs" key={index}>
+              <tr className="song" key={index} onClick={() => this.handleSongClick(song)}>
                 <td className="song-number">{index + 1}</td>
                 <td className="song-title">{song.title}</td>
                 <td className="song-duration">{song.duration}</td>
